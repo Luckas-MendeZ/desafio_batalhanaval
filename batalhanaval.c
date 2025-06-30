5de70e9ab90e107cd5f087edbfeb7d48df9aac2b
Luckas-MendeZ <lucasmendesdev47@gmail.com>
2025-06-29 22:36:31 -0300

    Adicionado: navios nas diagonais e comentarios. Melhorado: legibilidade e execuçao do codigo.

--- batalhanaval.c
+++ batalhanaval.c
@@ -4,7 +4,19 @@
 #define OCUPADO 3 // valor para marcar os navios para evitar sobreposição
 #define AGUA 0 // valor que representa a agua
 
+// Função para imprimir o tambuleiro no console
+void imprimir_tabuleiro(int mar [TAM][TAM]){
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d ", mar[i][j]); // Imprime valor da célula com espaço para alinhamento
+        }
+        printf("\n"); // Quebra de linha ao final de cada linha do tabuleiro
+    }
+}
 int main(){
+    // Tabuleiro com alguns navios já posicionados ( Navio horizontal linha 1) ( Navio vertical coluna 7)
     int mar [10][10] = {
         {0,0,0,0,0,0,0,0,0,0},
         {0,0,0,3,3,3,0,0,0,0},
@@ -18,15 +30,39 @@ int main(){
         {0,0,0,0,0,0,0,3,0,0}
     };
 
-    for (int i = 0; i < 10; i++)
+    // Coordenadas iniciais para o navio diagonal principal
+    int linha = 4;
+    int coluna = 2;
+
+    // Verifica se o navio cabe no tabuleiro na diagonal principal e se as posições estão livres
+    if (linha + 2 < TAM && coluna + 2 < TAM)
     {
-        for (int j = 0; j < 10; j++)
-        {
-            printf("%d", mar[i][j]);
-        }
-        printf("\n");
+        if (mar[linha][coluna] == AGUA &&
+        mar[linha+1][coluna+1] == AGUA &&
+        mar[linha+2][coluna+2] == AGUA)
+    {
+        // Marca as posições do navio
+        mar[linha][coluna] = OCUPADO;
+        mar[linha+1][coluna+1] = OCUPADO;
+        mar[linha+2][coluna+2] = OCUPADO;
+    }
+    }
+
+    int linha2 = 0;
+    int coluna2 = 9;
+    if (linha2 + 2 < TAM && coluna2 - 2 >= 0)
+    {
+        if (mar[linha2][coluna2] == AGUA &&
+        mar[linha2+1][coluna2-1] == AGUA &&
+        mar[linha2+2][coluna2-2] == AGUA)
+
+    {  
+        mar[linha2][coluna2] = OCUPADO;
+        mar[linha2+1][coluna2-1] = OCUPADO;
+        mar[linha2+2][coluna2-2] = OCUPADO;
+    }
     }
     
-
+    imprimir_tabuleiro(mar);
     return 0;
 }
